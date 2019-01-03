ec7f634f0884f50a0c62a095fb9ac7e674c8e1aa
Wayne Kenney <wayneleokenney@gmail.com>
2019-01-03 18:03:48 -0500

    some basic commands for excel xlsx

--- first.py
+++ first.py
@@ -2,4 +2,14 @@ import xlrd
 
 ti = xlrd.open_workbook("Titles.xlsx")
 
+print(ti.nsheets)
+print(ti.sheet_names())
+
 t11 = ti.sheet_by_index(0)
+
+print(t11.row_values(0))
+print(t11.col_values(0))
+print(t11.cell(0,0))
+print(t11.cell(0,0).value)
+print(t11.row_slice(rowx=0,start_colx=0,end_colx=2))
+
