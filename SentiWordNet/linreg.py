59e3d9474db25790eff117dd9e165d30ec7ebdef
Wayne Kenney <wayneleokenney@gmail.com>
2019-03-07 10:06:40 -0500

    changed response to continuous

--- SentiWordNet/linreg.py
+++ SentiWordNet/linreg.py
@@ -1,28 +1,28 @@
 import pandas as pd
 import statsmodels.api as sm
 import numpy as np
-import statsmodels.formula.api as smf
 
 pd.options.display.float_format = '{:20,.3f}'.format
 
-art = pd.read_pickle("art_m.pkl")
-su = pd.read_excel("Summaries.xlsx",nrows=1000)
+art = pd.read_pickle("art_all_m.pkl")
+# su = pd.read_excel("Summaries.xlsx",nrows=1000)
 
-index = []
-for i in range(1000):
-	cont = su.loc[i,'Classification'] == 'controversial'
-	ncont = su.loc[i,'Classification'] == 'not controversial'
-	if cont or ncont:
-		index += [i]
+# index = []
+# for i in range(1000):
+# 	cont = su.loc[i,'Classification'] == 'controversial'
+# 	ncont = su.loc[i,'Classification'] == 'not controversial'
+# 	if cont or ncont:
+# 		index += [i]
 
-su = su.iloc[index]
+# su = su.iloc[index]
 
-def prob(row):
-	margin = row['controversial']+row['not controversial']
-	new = row['controversial']/margin
-	return(new)
+# def prob(row):
+# 	new = row['controversial']-row['not controversial']
+# 	return(new)
 
-y = su.apply(prob,axis=1)
+# y = su.apply(prob,axis=1)
+
+
 def remove_error(data):
 	no_error = []
 	for i in data.index:
@@ -32,17 +32,25 @@ def remove_error(data):
 
 clean_loc = remove_error(art)
 x = art.loc[clean_loc,:]
-x = sm.add_constant(x)
 
-y = y.astype(float).loc[clean_loc]
+y = x["Response"].astype(float).loc[clean_loc]
+
+def abs_sum(row):
+	return(abs(row["P-N Metric"]))
+
+x["Abs(P-N) Metric"] = x.apply(abs_sum,axis=1)
+
+x['Response'] = x['Response'].apply(lambda x: 1/(1-np.exp(-x)))
+
 
 ############################
 #analysis
 
-cols = ["const", "Pos Score","Neg Score","Word Count","P/N Metric"]
+cols = ["Neg Score","Pos Score","P+N Metric","Abs(P-N) Metric", "Word Count"]
 x = x.loc[:,cols]
-
 x = x.astype(float)
 
+
+x = sm.add_constant(x)
 model = sm.OLS(y,x).fit()
 print(model.summary())
--- SentiWordNet/pull_offical_data.py
+++ SentiWordNet/pull_offical_data.py
@@ -1,29 +1,31 @@
 import pandas as pd
 
-# art = pd.read_csv("articles_official.csv",usecols=[0,4,5,6,7,10,15]).iloc[:73328]
+# art = pd.read_csv("articles_official.csv",usecols=[0,4,5,6,7,10,15]).iloc[:7844]
 
 # art.to_pickle("art_off.pkl")
 
-# art = pd.read_pickle("art_off.pkl")
+art = pd.read_pickle("art_off.pkl")
 # art_id = pd.read_excel("Titles.xlsx",usecols=[0,1])
 # rows = [i-1 for i in art_id['Art_Id']]
 
 # art.loc[rows].to_pickle("art.pkl")
 
-# art = pd.read_pickle('art.pkl')
 su = pd.read_excel("Summaries.xlsx",nrows=1000)
+idd = [i-1 for i in su['DB ID']]
+su.index = idd
+art_all = art.loc[idd,:]
 
-print(sum(su["Classification"].value_counts()))
+# print(sum(su["Classification"].value_counts()))
 
-index = []
-for i in range(1000):
-	cont = su.loc[i,'Classification'] == 'controversial'
-	ncont = su.loc[i,'Classification'] == 'not controversial'
-	if cont or ncont:
-		index += [i]
+# index = []
+# for i in range(1000):
+# 	cont = su.loc[i,'Classification'] == 'controversial'
+# 	ncont = su.loc[i,'Classification'] == 'not controversial'
+# 	if cont or ncont:
+# 		index += [i]
 
-su = su.iloc[index]
-su["Classification"].value_counts()
+# su = su.iloc[index]
+# su["Classification"].value_counts()
 
 
 # art = art.iloc[index]
@@ -33,21 +35,21 @@ su["Classification"].value_counts()
 
 # art['Response'] = su["Classification"]
 
-# from single_document import swn_single
+from single_document import swn_single
 
-# articles = art
+articles = art_all
 
-# swn_metric = pd.DataFrame()
-# for i in articles.index:
-# 	title_body = str(articles.loc[i,'content']) + str(articles.loc[i,'title'])
-# 	data = swn_single(title_body)
+swn_metric = pd.DataFrame()
+for i in articles.index:
+	title_body = str(articles.loc[i,'content']) + str(articles.loc[i,'title'])
+	data = swn_single(title_body)
 
-# 	data['Response'] = articles.loc[i,'Response']
-# 	data['Title'] = articles.loc[i,'title']
-# 	data['Body'] = articles.loc[i,'content']
-# 	data['Summary'] = articles.loc[i,'summary']
-# 	st = pd.DataFrame(data=data,index=[i])
-# 	swn_metric = swn_metric.append(st)
-# 	print(999-i)
+	# data['Response'] = articles.loc[i,'Response']
+	data['Response'] = su.loc[i,'controversial']-su.loc[i,'not controversial']
+	data['Title'] = articles.loc[i,'title']
+	data['Body'] = articles.loc[i,'content']
+	data['Summary'] = articles.loc[i,'summary']
+	st = pd.DataFrame(data=data,index=[i])
+	swn_metric = swn_metric.append(st)
 
-# swn_metric.to_pickle("art_m.pkl")
+swn_metric.to_pickle("art_all_m.pkl")
