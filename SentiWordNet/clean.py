3cd7bf13baa42a710131d76b1093eb3b8a15093c
Wayne Kenney <wayneleokenney@gmail.com>
2019-03-12 12:34:15 -0400

    update

--- SentiWordNet/clean.py
+++ SentiWordNet/clean.py
@@ -5,16 +5,10 @@ from textblob import Word
 import string
 
 def clean(x):
-	def clean2(text):
-		chars = string.punctuation + "0123456789"
-		for i in chars:
-			text = text.replace(i," ")
-		return(text)
-
 	x = str(x)
-	x = x.lower()
 	x = unicodedata.normalize("NFKD", x)
-	x = clean2(x)
-	li = x.split()
-	li = [x for x in li if len(x) > 2]
-	return(li)
+	x = x.lower()
+	chars = string.punctuation + "0123456789"
+	for i in chars:
+		x = x.replace(i," ")
+	return(x)
--- SentiWordNet/tfidf.py
+++ SentiWordNet/tfidf.py
@@ -10,7 +10,7 @@ from gensim.utils import SaveLoad
 from clean import clean
 from math import log
 from nltk.corpus import sentiwordnet as swn
-from nltk import pos_tag
+from nltk import pos_tag, word_tokenize
 
 art = pd.read_pickle("art.pkl")
 su = pd.read_excel("Summaries.xlsx",nrows=1000)
@@ -19,22 +19,24 @@ su.index = art.index
 ####################################################################################
 #####################################################################################
 #make dic
-labeled_sentances = []
-k = 1001
-for index,row in art.iterrows():
-	k -= 1
-	if k % 100 ==0:
-		print(k)
-	try:
-		body = str(row['content'])+str(row['title'])
-		cleaned = clean(body)
-		labeled_sentances.append(cleaned)
-	except:
-		print('Error Index:',index)
+# labeled_sentances = []
+# k = 1001
+# for i in art.index:
+# 	k -= 1
+# 	if k % 100 ==0:
+# 		print(k)
+# 	try:
+# 		body = str(art.loc[i,'content'])+ " " +str(art.loc[i,'title'])
+# 		cleaned = clean(body)
+# 		cleaned = word_tokenize(cleaned)
+# 		labeled_sentances.append(cleaned)
+# 	except:
+# 		print('Error Index:',index)
 
-dct = Dictionary(labeled_sentances)  # fit dictionary
 
-dct.save("doc.dic")
+# dct = Dictionary(labeled_sentances)  # fit dictionary
+
+# dct.save("doc.dic")
 
 
 ####################################################################################
@@ -45,52 +47,64 @@ dct.save("doc.dic")
 dct = SaveLoad.load("doc.dic")
 
 def single_doc(doc_tokenized):
+	tags = pos_tag(doc_tokenized)
+
 	pos_score, neg_score = 0,0
 	total_doc_freq = 0
-	for word in doc_tokenized:
-		pos_tag = word.pos_tag()
-		print(pos_tag)
-		break
+	for t in tags:
+		word = t[0]
+		pos = t[1][0]
+
 		word_synsets = list(swn.senti_synsets(word))
-		pos_syn_score, neg_syn_score = 0, 0
-		if len(word_synsets) != 0:
-			for syn in word_synsets:
-				pos_syn_score += syn.pos_score()
-				neg_syn_score += syn.neg_score()
+		print(word_synsets)
+		break
+		# pos_syn_score, neg_syn_score = 0, 0
+		# if len(word_synsets) != 0:
+		# 	for syn in word_synsets:
+		# 		pos_syn_score += syn.pos_score()
+		# 		neg_syn_score += syn.neg_score()
 
-			pos_syn_score /= len(word_synsets)
-			neg_syn_score /= len(word_synsets)
+	# 		pos_syn_score /= len(word_synsets)
+	# 		neg_syn_score /= len(word_synsets)
 
-			token_id = dct.token2id[word]
-			unique_doc_freq = 1000-dct.dfs[token_id]
-			total_doc_freq += unique_doc_freq
+	# 		token_id = dct.token2id[word]
+	# 		unique_doc_freq = 1000-dct.dfs[token_id]
+	# 		total_doc_freq += unique_doc_freq
 
-			pos_syn_score *= unique_doc_freq
-			neg_syn_score *= unique_doc_freq
+	# 		pos_syn_score *= unique_doc_freq
+	# 		neg_syn_score *= unique_doc_freq
 
-		pos_score += pos_syn_score
-		neg_score += neg_syn_score
+	# 	pos_score += pos_syn_score
+	# 	neg_score += neg_syn_score
 
-	pos_score /= len(doc_tokenized)
-	neg_score /= len(doc_tokenized)
+	# pos_score /= len(doc_tokenized)
+	# neg_score /= len(doc_tokenized)
 
-	docu_ave = {"Word Count": len(doc_tokenized),"P+N Metric": pos_score + neg_score, "Abs(P-N) Metric": abs(pos_score - neg_score), \
-	             "Pos Score": pos_score,"Neg Score": neg_score}
+	# docu_ave = {"Word Count": len(doc_tokenized),"P+N Metric": pos_score + neg_score, "Abs(P-N) Metric": abs(pos_score - neg_score), \
+	#              "Pos Score": pos_score,"Neg Score": neg_score}
 
-	return(docu_ave)
+	# return(docu_ave)
 
-swn_metric = pd.DataFrame()
-k = 1001
-for i in art.index:
-	title_body = clean(str(art.loc[i,'title']) + str(art.loc[i,'content']))
-	data = single_doc(title_body)
+# swn_metric = pd.DataFrame()
+# k = 1001
+# for i in art.index:
+# 	title_body = clean(str(art.loc[i,'title']) + " " + str(art.loc[i,'content']))
+# 	title_body = word_tokenize(title_body)
+# 	try:
+# 		data = single_doc(title_body)
+# 	except Exception as error:
+# 		print("Error:", error,"\n","Error ind:",i)
 
-	data['Response'] = su.loc[i,'controversial']-su.loc[i,'not controversial']
-	data['Body'] = art.loc[i,'content']
-	st = pd.DataFrame(data=data,index=[i])
-	swn_metric = swn_metric.append(st)
-	k -= 1
-	if k % 100 == 0:
-		print(k)
+# 	data['Response'] = su.loc[i,'controversial']-su.loc[i,'not controversial']
+# 	data['Body'] = art.loc[i,'content']
+# 	st = pd.DataFrame(data=data,index=[i])
+# 	swn_metric = swn_metric.append(st)
+# 	k -= 1
+# 	if k % 100 == 0:
+# 		print(k)
 
-swn_metric.to_pickle("art_idf_m.pkl")
+# swn_metric.to_pickle("art_idf_m.pkl")
+
+title_body = clean(str(art.loc[53,'title']) + " " + str(art.loc[53,'content']))
+title_body = word_tokenize(title_body)
+single_doc(title_body)
